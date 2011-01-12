12e4a55a39e8fbb0e3f2378c4646fe0414e290fe
R. Potter <rjspotter@gmail.com>
2011-01-12 15:00:13 -0800

    scheme and font stuff

--- init.el
+++ init.el
@@ -16,6 +16,8 @@
 (setq make-backup-files nil)     ;no backup files
 (setq default-directory "~/code/" )
 (tool-bar-mode -1)
+(set-default-font "-artwiz-snap-normal-normal-normal-*-10-*-*-*-*-90-iso10646-1")
+(modify-frame-parameters nil '((wait-for-wm . nil)))
 
 (when
     (load
@@ -37,6 +39,52 @@
 (require 'autopair)
 (autopair-global-mode 1)
 (setq autopair-autowrap t)
+
+;;
+;; SLIME !!!
+;;
+;(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-lisp-implementations
+     '(
+        (mit-scheme ("/usr/bin/mit-scheme") :init mit-scheme-init)
+        (sbcl ("/usr/bin/sbcl") :coding-system utf-8-unix)))
+(add-to-list 'load-path "/usr/share/emacs-snapshot/site-lisp/slime/")
+(add-to-list 'load-path "/usr/share/emacs-snapshot/site-lisp/slime/contrib/")
+(add-hook 'slime-load-hook (lambda () (require 'slime-scheme)))
+(require 'slime)
+(slime-setup)
+
+(defun mit-scheme-init (file encoding)
+  (format "%S\n\n"
+	  `(begin
+	    (load-option 'format)
+	    (load-option 'sos)
+	    (eval 
+	     '(construct-normal-package-from-description
+	       (make-package-description '(swank) '(()) 
+					 (vector) (vector) (vector) false))
+	     (->environment '(package)))
+	    (load ,(expand-file-name 
+		    "~/.emacs.d/modes/swank-mit-scheme.scm" ; <-- insert your path
+		    slime-path)
+		  (->environment '(swank)))
+	    (eval '(start-swank ,file) (->environment '(swank))))))
+
+(defun mit-scheme ()
+  (interactive)
+  (slime 'mit-scheme))
+
+(defun find-mit-scheme-package ()
+  (save-excursion
+    (let ((case-fold-search t))
+      (and (re-search-backward "^[;]+ package: \\((.+)\\).*$" nil t)
+	   (match-string-no-properties 1)))))
+
+(setq slime-find-buffer-package-function 'find-mit-scheme-package)
+
+;
+; !!! End SLIME
+;
 
 (autoload 'company-mode "company" nil t)
 
