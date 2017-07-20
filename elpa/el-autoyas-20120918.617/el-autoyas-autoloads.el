18a0d59443a7af007a11c612ada406159c9625cb
R. Potter <rjspotter@gmail.com>
2017-07-20 09:30:17 -0700

    adding json-mode and upgrading others

---
+++ elpa/el-autoyas-20120918.617/el-autoyas-autoloads.el
@@ -0,0 +1,27 @@
+;;; el-autoyas-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "el-autoyas" "el-autoyas.el" (0 0 0 0))
+;;; Generated autoloads from el-autoyas.el
+
+(autoload 'el-autoyas-enable "el-autoyas" "\
+Load and activate package.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "el-autoyas" '("el-autoyas-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("el-autoyas-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; el-autoyas-autoloads.el ends here
