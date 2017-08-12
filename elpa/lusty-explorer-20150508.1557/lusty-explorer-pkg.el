e0855b5dc99d19d2f6169ba1a3e3c16f667b04fa
R. Potter <rjspotter@gmail.com>
2017-08-12 13:15:04 -0700

    upgrade js2 / lusty-explorer to elpa managed and ran update

---
+++ elpa/lusty-explorer-20150508.1557/lusty-explorer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lusty-explorer" "20150508.1557" "Dynamic filesystem explorer and buffer switcher" 'nil :commit "a6e78cafc193050a493d16d07eae107a2436c293" :keywords '("convenience" "files" "matching"))
