09d2195008cf12ceb4dd2b212f0774c8f5b0b783
R. Potter <rjspotter@gmail.com>
2018-03-17 10:56:32 -0600

    Update & add typescript support

---
+++ elpa/ts-comint-20171105.2247/ts-comint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ts-comint" "20171105.2247" "Run a Typescript interpreter in an inferior process window." 'nil :commit "8817dc7b3a6eb78c3cad42e5677c2113274a1963" :url "https://github.com/josteink/ts-comint" :keywords '("typescript" "node" "inferior-mode" "convenience"))
---
+++ elpa/yasnippet-20180317.431/yasnippet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yasnippet" "20180317.431" "Yet another snippet extension for Emacs." '((cl-lib "0.5")) :commit "ae9581028942dd0237a007fdc0a49a76ef8c3e11" :url "http://github.com/joaotavora/yasnippet" :keywords '("convenience" "emulation"))
