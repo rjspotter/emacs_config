7756d26d099ee91d0a8f751d519a08560a5af8e9
R. Potter <rjspotter@gmail.com>
2018-04-28 11:46:36 -0600

    update && add projectile

---
+++ elpa/projectile-rails-0.16.0/projectile-rails-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile-rails" "0.16.0" "Minor mode for Rails projects based on projectile-mode" '((emacs "24.3") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2")) :commit "31c9f90d472e07cb8e49fa8992b187b67c9c9a71" :keywords '("rails" "projectile") :url "https://github.com/asok/projectile-rails")
