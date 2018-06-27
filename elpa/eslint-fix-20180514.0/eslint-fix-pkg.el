3e5d1a9438c1ef2f6d9f6315fa91344cce800130
R. Potter <rjspotter@gmail.com>
2018-06-27 21:30:47 +0300

    Let's get some eslint & Prettier going on

---
+++ elpa/eslint-fix-20180514.0/eslint-fix-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "eslint-fix" "20180514.0" "Fix JavaScript files using ESLint" 'nil :commit "f81f3b47a47460611fbdbdae1d23275ec78f2f8d" :keywords '("tools" "javascript" "eslint" "lint" "formatting" "style") :url "https://github.com/codesuki/eslint-fix")
