07b0ee890adf8e4fe37817159562a981a2d7761f
R. Potter <rjspotter@gmail.com>
2018-08-08 22:57:43 +0000

    Update 2018.08.08 also added utop

---
+++ elpa/utop-20180706.2249/utop-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "utop" "20180706.2249" "Universal toplevel for OCaml" '((emacs "24")) :commit "9612274567743988d1f94908c3654c7fd418f387" :keywords '("ocaml" "languages") :authors '(("Jeremie Dimino" . "jeremie@dimino.org")) :maintainer '("Jeremie Dimino" . "jeremie@dimino.org") :url "https://github.com/diml/utop")
