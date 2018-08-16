cbad0601cc3c969d78a3f9ffb5d268bf3a521905
R. Potter <rjspotter@gmail.com>
2018-08-16 23:00:56 +0000

    update 2018.08.13

---
+++ elpa/indium-20180814.1614/indium-autoloads.el
@@ -0,0 +1,65 @@
+;;; indium-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "indium-interaction" "indium-interaction.el"
+;;;;;;  (23411 17134 993779 664000))
+;;; Generated autoloads from indium-interaction.el
+
+(autoload 'indium-connect "indium-interaction" "\
+Open a new connection to a runtime.
+
+\(fn)" t nil)
+
+(autoload 'indium-launch "indium-interaction" "\
+Start a new process and connect to it.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "indium-list-sources" "indium-list-sources.el"
+;;;;;;  (23411 17134 789785 298000))
+;;; Generated autoloads from indium-list-sources.el
+
+(autoload 'indium-list-sourcemap-sources "indium-list-sources" "\
+Display a list of all resolved sourcemap sources.
+
+\(fn)" t nil)
+
+(autoload 'indium-list-script-sources "indium-list-sources" "\
+Display a list of all resolved script sources.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "indium-scratch" "indium-scratch.el" (23411
+;;;;;;  17134 841783 861000))
+;;; Generated autoloads from indium-scratch.el
+
+(autoload 'indium-scratch "indium-scratch" "\
+Pop to the scratch buffer.
+If no scratch buffer exists for the current connection, create
+one first.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("indium-breakpoint.el" "indium-chrome.el"
+;;;;;;  "indium-client.el" "indium-debugger-litable.el" "indium-debugger-locals.el"
+;;;;;;  "indium-debugger.el" "indium-faces.el" "indium-inspector.el"
+;;;;;;  "indium-nodejs.el" "indium-pkg.el" "indium-render.el" "indium-repl.el"
+;;;;;;  "indium-seq-fix.el" "indium-structs.el" "indium.el") (23411
+;;;;;;  17135 17779 1000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; indium-autoloads.el ends here
