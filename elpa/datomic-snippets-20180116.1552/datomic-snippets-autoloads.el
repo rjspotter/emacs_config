0a24d00ef170302d7a3dd0e2b617feffe9518109
R. Potter <rjspotter@gmail.com>
2018-08-20 22:37:14 +0000

    update 2018.08.20

--- elpa/datomic-snippets-20180116.1552/datomic-snippets-autoloads.el
+++
@@ -1,34 +0,0 @@
-;;; datomic-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "datomic-snippets" "datomic-snippets.el" (23400
-;;;;;;  45681 578553 485000))
-;;; Generated autoloads from datomic-snippets.el
-
-(autoload 'datomic-snippets-show-docs-at-point "datomic-snippets" "\
-
-
-\(fn)" t nil)
-
-(autoload 'datomic-snippets-initialize "datomic-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load "yasnippet" '(datomic-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("datomic-snippets-pkg.el") (23400 45681
-;;;;;;  574553 593000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; datomic-snippets-autoloads.el ends here
