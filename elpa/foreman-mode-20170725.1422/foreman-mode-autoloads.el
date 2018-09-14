72077e122b2d5348237260429214e455585fc38e
R. Potter <rjspotter@gmail.com>
2018-09-14 22:23:46 +0000

    Update 2018.09.14
    clean up some keybindings add rspec mode (not configured yet)

---
+++ elpa/foreman-mode-20170725.1422/foreman-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; foreman-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "foreman-mode" "foreman-mode.el" (23441 38066
+;;;;;;  587038 574000))
+;;; Generated autoloads from foreman-mode.el
+
+(autoload 'foreman "foreman-mode" "\
+
+
+\(fn)" t nil)
+
+(autoload 'foreman-start "foreman-mode" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; foreman-mode-autoloads.el ends here
