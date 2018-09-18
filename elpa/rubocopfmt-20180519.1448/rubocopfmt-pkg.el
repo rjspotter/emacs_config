dcd015baece39219b569474c5d8a8e65b7a0e9de
R. Potter <rjspotter@gmail.com>
2018-09-18 23:48:15 +0000

    update 2018.09.18 && add rubocop

---
+++ elpa/rubocopfmt-20180519.1448/rubocopfmt-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rubocopfmt" "20180519.1448" "Minor-mode to format Ruby code with RuboCop on save" '((cl-lib "0.5")) :commit "34c69c9c923d0da223f7569a6ecc842095adcf85" :keywords '("convenience" "wp" "edit" "ruby" "rubocop") :authors '(("Jim Myhrberg")) :maintainer '("Jim Myhrberg") :url "https://github.com/jimeh/rubocopfmt.el")
