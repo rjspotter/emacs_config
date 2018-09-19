329c268b903c749eb35749ddc6937b812538023a
R. Potter <rjspotter@gmail.com>
2018-09-19 00:03:40 +0000

    getting modes from melpa rather than locally

---
+++ elpa/haskell-snippets-20160919.22/haskell-snippets-autoloads.el
@@ -0,0 +1,29 @@
+;;; haskell-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "haskell-snippets" "haskell-snippets.el" (23457
+;;;;;;  37276 821752 610000))
+;;; Generated autoloads from haskell-snippets.el
+
+(autoload 'haskell-snippets-initialize "haskell-snippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(haskell-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("haskell-snippets-pkg.el") (23457 37276
+;;;;;;  833752 266000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; haskell-snippets-autoloads.el ends here
