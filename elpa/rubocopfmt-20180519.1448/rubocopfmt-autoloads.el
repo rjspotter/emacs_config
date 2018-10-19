7ebc2c6fe3cd14b2d917ee416ae55fd2c3fc9a82
R. Potter <rjspotter@gmail.com>
2018-10-19 16:36:38 -0600

    upgrade 2018.10.19

--- elpa/rubocopfmt-20180519.1448/rubocopfmt-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; rubocopfmt-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "rubocopfmt" "rubocopfmt.el" (23457 36373 287656
-;;;;;;  600000))
-;;; Generated autoloads from rubocopfmt.el
-
-(autoload 'rubocopfmt "rubocopfmt" "\
-Format the current buffer with rubocop.
-
-\(fn)" t nil)
-
-(autoload 'rubocopfmt-mode "rubocopfmt" "\
-Enable format-on-save for `ruby-mode' buffers via rubocopfmt.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rubocopfmt-autoloads.el ends here
