a861cc4db7fecbb9a109e39b43fa989c53052d39
R. Potter <rjspotter@gmail.com>
2018-10-25 15:48:06 -0600

    configuring tide

--- init.el
+++ init.el
@@ -243,6 +243,23 @@
 (setq typescript-indent-level 2
       typescript-expr-indent-offset 2)
 
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+;;(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
 ;;Coffee
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
