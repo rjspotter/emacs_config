35d9a39c02033898b1e4f5cd4eb26ea9b20075fe
R. Potter <rjspotter@gmail.com>
2019-10-25 19:55:39 -0600

    still tweaking

---
+++ elpa/sql-presto-20190113.1742/sql-presto-autoloads.el
@@ -0,0 +1,24 @@
+;;; sql-presto-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "sql-presto" "sql-presto.el" (23987 42532 27694
+;;;;;;  576000))
+;;; Generated autoloads from sql-presto.el
+
+(autoload 'sql-presto "sql-presto" "\
+Run Presto as an inferior process.
+
+The buffer with name BUFFER will be used or created.
+
+\(fn &optional BUFFER)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sql-presto-autoloads.el ends here
---
+++ elpa/sql-presto-20190113.1742/sql-presto-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sql-presto" "20190113.1742" "No description available." 'nil :commit "bcda455e300a1af75c7bb805882329bc844703b2")
