1074fbb59dd93eea73d9619eda048650c9e2c75a
R. Potter <rjspotter@gmail.com>
2019-12-12 18:21:34 +0000

    updating 2019.12.12

---
+++ elpa/julia-repl-20191209.1051/julia-repl-pkg.el
@@ -0,0 +1,13 @@
+(define-package "julia-repl" "20191209.1051" "A minor mode for a Julia REPL"
+  '((emacs "25")
+    (s "1.12"))
+  :keywords
+  '("languages")
+  :authors
+  '(("Tamas Papp" . "tkpapp@gmail.com"))
+  :maintainer
+  '("Tamas Papp" . "tkpapp@gmail.com")
+  :url "https://github.com/tpapp/julia-repl")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lusty-explorer-20191203.1635/lusty-explorer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lusty-explorer" "20191203.1635" "Dynamic filesystem explorer and buffer switcher" 'nil :commit "1fbd5cfbdbb8ed744b5a141648febf620bbf5a84" :keywords '("convenience" "files" "matching" "tools") :url "https://github.com/sjbach/lusty-emacs")
