6ab973b5564ee6f06e181e1da615073390d6d32e
R. Potter <rjspotter@gmail.com>
2020-01-19 19:10:59 +0000

    adding some company backends and configs

---
+++ elpa/company-nginx-20180604.2/company-nginx-autoloads.el
@@ -0,0 +1,25 @@
+;;; company-nginx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "company-nginx" "company-nginx.el" (0 0 0 0))
+;;; Generated autoloads from company-nginx.el
+
+(autoload 'company-nginx-keywords "company-nginx" "\
+Add Nginx directive keywords to company-keywords-alist.
+
+\(fn)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-nginx-autoloads.el ends here
