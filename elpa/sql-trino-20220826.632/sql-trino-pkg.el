4ada0182cd1a8e0229e89aace40d027922bcd00c
R. Potter <rjspotter@gmail.com>
2024-06-30 14:52:11 -0600

    usual updates

---
+++ elpa/sql-trino-20220826.632/sql-trino-pkg.el
@@ -0,0 +1,14 @@
+(define-package "sql-trino" "20220826.632" "Adds Trino support to SQLi mode"
+  '((emacs "24.4"))
+  :commit "624a879ec0d03cae8a92f26d21d88c831e15eb41" :authors
+  '(("Filipe Regadas" . "oss@regadas.email"))
+  :maintainers
+  '(("Filipe Regadas" . "oss@regadas.email"))
+  :maintainer
+  '("Filipe Regadas" . "oss@regadas.email")
+  :keywords
+  '("tools")
+  :url "https://github.com/regadas/sql-trino")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
