70dcfc714c406b7c2e005670a077869fe7a69eb6
R. Potter <rjspotter@intentionally-blank.com>
2025-06-19 15:29:09 -0600

    add mise and recompile

---
+++ elpa/mise-0.3.1/mise-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "mise" "0.3.1"
+  "Support for `mise' cli."
+  '((emacs      "29.1")
+    (inheritenv "0.2")
+    (dash       "2.19.1"))
+  :url "https://github.com/eki3z/mise.el"
+  :commit "2ba633b84c5d678ef128901c443ee43f6052d3de"
+  :revdesc "2ba633b84c5d"
+  :keywords '("tools" "processes")
+  :authors '(("Eki Zhang" . "liuyinz95@gmail.com"))
+  :maintainers '(("Eki Zhang" . "liuyinz95@gmail.com")))
